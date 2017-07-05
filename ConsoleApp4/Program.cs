f0ff3044aa6e76a25eea6ceaa001b197a8f70e0e
benediktus15 <benedyctoes@gmail.com>
2017-07-05 10:49:56 +0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, -2, -3, 5 };
+            var posNums = from n in nums orderby n descending select n * 4 / 2;
+            Console.Write("nilai: ");
+            foreach (int i in posNums)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
